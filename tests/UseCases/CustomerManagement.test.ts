edc2e9084835fc9018836fbe6176316dcee3300d
saugatdai <saugatdai@gmail.com>
2021-02-11 17:05:22 +0545

    Homeworking Commit before college pull

--- tests/UseCases/CustomerManagement.test.ts
+++ tests/UseCases/CustomerManagement.test.ts
@@ -4,74 +4,77 @@ import CustomerManager from '../../src/UseCases/CustomerManagementComponent/Cust
 import Token from '../../src/Entities/TokenCore/Token';
 
 describe('Testing of customer management compoenent', () => {
-    const token1: Token = {
-        tokenId: 1,
-        date: new Date(),
-        tokenNumber: 2
-    }
+  const token1: Token = {
+    tokenId: 1,
+    date: new Date(),
+    tokenNumber: 2,
+  };
 
-    const customer1 : Customer = {
-        customerId: 123,
-        token: token1,
-        remarks: "-"
-    }
+  const customer1: Customer = {
+    customerId: 123,
+    token: token1,
+    remarks: '-',
+  };
 
-    const token2: Token = {
-        tokenId: 1,
-        date: new Date(),
-        tokenNumber: 2
-    }
+  const token2: Token = {
+    tokenId: 1,
+    date: new Date(),
+    tokenNumber: 2,
+  };
 
-    const customer2 : Customer = {
-        customerId: 123,
-        token: token2,
-        remarks: "-"
-    }
+  const customer2: Customer = {
+    customerId: 123,
+    token: token2,
+    remarks: '-',
+  };
 
-    const addCustomerMockFunction = jest.fn();
-    const addCustomerFunction = (customer: Customer) => {addCustomerMockFunction()};
+  const addCustomerMockFunction = jest.fn();
+  const addCustomerFunction = (customer: Customer) => {
+    addCustomerMockFunction();
+  };
 
-    const getCustomerByIdMockFunction = jest.fn();
-    const getCustomerByIdFunction = (id: number) => customer2;
+  const getCustomerByIdMockFunction = jest.fn();
+  const getCustomerByIdFunction = (id: number) => customer2;
 
-    const updateCustomerMockFunction = jest.fn();
-    const updateCustomerFunction = (customer: Customer) => {updateCustomerMockFunction()};
+  const updateCustomerMockFunction = jest.fn();
+  const updateCustomerFunction = (customer: Customer) => {
+    updateCustomerMockFunction();
+  };
 
-    const deleteCustomerMockFunction = jest.fn();
-    const deleteCustomerFunction = (id: number) => {deleteCustomerMockFunction()};
+  const deleteCustomerMockFunction = jest.fn();
+  const deleteCustomerFunction = (id: number) => {
+    deleteCustomerMockFunction();
+  };
 
-    const getAllCustomersFunction = () => [customer1, customer2];
+  const getAllCustomersFunction = () => [customer1, customer2];
 
-    const customerStorageInteractorAdapter: CustomerStorageInteractorAdapter = {
-        addCustomer: addCustomerMockFunction,
-        getCustomerById: getCustomerByIdFunction,
-        updateCustomer: updateCustomerFunction,
-        deleteCustomerById: deleteCustomerFunction,
-        getAllCustomers: getAllCustomersFunction
-    }
+  const customerStorageInteractorAdapter: CustomerStorageInteractorAdapter = {
+    addCustomer: addCustomerMockFunction,
+    getCustomerById: getCustomerByIdFunction,
+    updateCustomer: updateCustomerFunction,
+    deleteCustomerById: deleteCustomerFunction,
+    getAllCustomers: getAllCustomersFunction,
+  };
 
-    const customerManager = new CustomerManager(customer1);
-    customerManager.customerStorageInteractorAdapter = customerStorageInteractorAdapter;
+  const customerManager = new CustomerManager(customer1);
+  customerManager.customerStorageInteractorAdapter = customerStorageInteractorAdapter;
 
-    it('Should add customer in storage', () => {
-        customerManager.store();
-        expect(addCustomerMockFunction.mock.calls.length).toEqual(1);
-    });
+  it('Should add customer in storage', () => {
+    customerManager.store();
+    expect(addCustomerMockFunction.mock.calls.length).toEqual(1);
+  });
 
-    it('should update customer in storage', () => {
-        customerManager.update();
-        expect(updateCustomerMockFunction.mock.calls.length).toBe(1);
-    });
+  it('should update customer in storage', () => {
+    customerManager.update();
+    expect(updateCustomerMockFunction.mock.calls.length).toBe(1);
+  });
 
-    it('should delete customer in storage', () => {
-        customerManager.delete();
-        expect(deleteCustomerMockFunction.mock.calls.length).toBe(1);
-    });
+  it('should delete customer in storage', () => {
+    customerManager.delete();
+    expect(deleteCustomerMockFunction.mock.calls.length).toBe(1);
+  });
 
-    it('It should validate the given customer', () => {
-        expect(customerManager.validateInfo()).toBe(undefined);
-
-    });
-
-
+  it('It should validate the given customer', () => {
+    expect(customerManager.validateInfo()).toBe(undefined);
+  });
 });
