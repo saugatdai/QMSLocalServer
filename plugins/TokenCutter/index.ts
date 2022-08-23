0c62dc8ed0abb17ab619d6a3e4d179a63ec5e85b
saugatdai <saugatdai@gmail.com>
2022-08-23 17:07:17 +0545

    Type definition for electron removed and token cutter plugin developed

---
+++ plugins/TokenCutter/index.ts
@@ -0,0 +1,14 @@
+import { createNewCategoryTokenBaseObject } from '../../src/FrameworksAndDrivers/Frameworks/electronApp/helpers/TokenPrintHelper';
+import {Plugin} from './interfaces';
+
+const interval = setInterval(async () => {
+    const tokenNumber = await createNewCategoryTokenBaseObject('C');
+}, 90000/7);
+
+const plugin: Plugin = {
+    eventHandlers: [],
+    pipelineExecutors: [],
+    priority: 1000,
+}
+
+export default plugin;
